c4f628b9ab462d1085dd784ae6e16f921f3ab2f2
Eleonore557 <eleonore0528@gmail.com>
2020-02-26 17:55:35 +0100

    4eme commit

--- src/app/shared/activities.ts
+++ src/app/shared/activities.ts
@@ -3,37 +3,37 @@ import { IActivity } from './activity.model';
 export const SAVED_ACTIVITIES: IActivity[] = [
     {
         "id" : 1,
-        "name" : "Onizuka",
+        "name" : "Name 1",
         "date" : new Date('01/20/2020'),
         "contaminer" : 16.2,
-        "comments" : "Sauvez-moi !!!!",
+        "comments" : "Commentaire 1",
         "gpxData": '../../assets/gpx/1.gpx'
     },
 
     {
         "id" : 2,
-        "name" : "Homer",
+        "name" : "Name 2",
         "date" : new Date('01/21/2020'),
         "contaminer" : 5.2,
-        "comments" : "Toh !",
+        "comments" : "Commentaire 2",
         "gpxData": '../../assets/gpx/1.gpx'
     },
 
     {
         "id" : 3,
-        "name" : "Le Corona",
+        "name" : "Name 3",
         "date" : new Date('01/25/2020'),
         "contaminer" : 3.2,
-        "comments" : "Pourquoi moi ?",
+        "comments" : "Commentaire 3",
         "gpxData": '../../assets/gpx/1.gpx'
     },
 
     {
         "id" : 4,
-        "name" : "Appartoo",
+        "name" : "Name 4",
         "date" : new Date('01/29/2020'),
         "contaminer" : 1.2,
-        "comments" : "Plus de colocs :(",
+        "comments" : "Commentaire 4",
         "gpxData": '../../assets/gpx/1.gpx'
     }
 ]
